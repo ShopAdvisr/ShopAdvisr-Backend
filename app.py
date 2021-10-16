a9f46dd68823f45db1a828f9dcfd3d0342779768
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2021-10-16 01:38:31 -0400

    Product suggestion class

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import json
+import base64
+from ProductSuggestion import ProductSuggestion
+
+
 def create_app():
     app = Flask(__name__)
+    productSuggestion = ProductSuggestion()
     cors = CORS(app)
     app.config['CORS_HEADERS'] = 'Content-Type'
 
     @app.route('/data', methods=["GET"])
     def check():
         data = json.loads(request.get_data().decode('UTF-8'))
+    @app.route('/prodsuck',methods = ["GET"])
+    def product_suggestion():
+
+        data = json.loads(request.get_data().decode('UTF-8'))
+        productSuggestion.run(base64.decodebytes(data['audio']))
+
 
 
     return app
+
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app = create_app()
