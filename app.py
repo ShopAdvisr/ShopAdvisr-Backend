2ba26219861ae5774ff4829cd057760bc1fa48de
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2021-10-16 12:29:33 -0400

    making it deployable on heroku

--- app.py
+++ app.py
@@ -4,28 +4,24 @@ import json
 import base64
 from ProductSuggestion import ProductSuggestion
 
-
-def create_app():
-    app = Flask(__name__)
-    productSuggestion = ProductSuggestion()
-    cors = CORS(app)
-    app.config['CORS_HEADERS'] = 'Content-Type'
-
-    @app.route('/data', methods=["GET"])
-    def check():
-        data = json.loads(request.get_data().decode('UTF-8'))
-    @app.route('/suggestions',methods = ["GET"])
-    def product_suggestion():
-
-        data = request.get_data()
-
-        return productSuggestion.run(base64.decodebytes(data))
+app = Flask(__name__)
+productSuggestion = ProductSuggestion()
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
+@app.route('/data', methods=["GET"])
+def check():
+    data = json.loads(request.get_data().decode('UTF-8'))
 
-    return app
+
+@app.route('/suggestions', methods=["GET"])
+def product_suggestion():
+    data = request.get_data()
+
+    return productSuggestion.run(base64.decodebytes(data))
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app = create_app()
     app.run(threaded=True, port=5000)
