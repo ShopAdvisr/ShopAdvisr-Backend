22da814732fbcfa4ddd504954fccb29eff13216f
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2021-10-16 22:21:41 -0400

    few more endpoint

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import json
 import base64
 from ProductSuggestion import ProductSuggestion
 import GCFunctions
+
 GCFunctions.init_import()
 
 app = Flask(__name__)
@@ -19,22 +20,25 @@ def check():
     data = json.loads(request.get_data().decode('UTF-8'))
 
 
-@app.route('/suggestions', methods=["GET","POST"])
+@app.route('/suggestions', methods=["GET", "POST"])
 def product_suggestion():
-
     data = request.get_data()
-
 
     return productSuggestion.run(base64.decodebytes(data))
 
-@app.route('/ocr',methods=["POST"])
+
+@app.route('/ocr', methods=["POST"])
 def ocr():
     data = request.get_data()
-    
-    results = GCFunctions.productQuery("image binary", data, 5)
-    return json.dumps(results)
+    return GCFunctions.productQuery("image binary", data, 5)
+
+
+@app.route('/search', methods=["POST"])
+def search():
+    data = request.get_data()
+    return {"items": GCFunctions.productQuery("labels", data.split(), 20)}
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
-
