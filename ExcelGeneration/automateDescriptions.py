ca734573b5a18a9a937aa04a081239948e80eda5
Noor-Nasri <noor.nasri@hotmail.com>
2021-10-17 03:53:54 -0500

    Finished automation script - requiring 3 computers to finish on time

--- ExcelGeneration/automateDescriptions.py
+++ ExcelGeneration/automateDescriptions.py
@@ -11,70 +11,78 @@ Chrome_options = Options()
 Chrome_options.add_argument("--headless")
 chcd = "C:\\Users\\Noor\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe"
 finalizedWrites = []
+allItems = []
 
-def getDescription(link):
-    with webdriver.Chrome(chcd, chrome_options=Chrome_options) as driver:
-        print(link)
-        driver.get(link)
-        wait = WebDriverWait(driver, 10)
+def getDescription(driver):
+    for a in range(40):
+        time.sleep(0.5)
+        
+        try:
+            time.sleep(0.25)
 
-        for a in range(40):
-            time.sleep(0.5)
-            try:
-                time.sleep(0.25)
+            driver.execute_script("window.scrollBy(0, -2000)")
+            time.sleep(0.25)
 
-                driver.execute_script("window.scrollBy(0, -2000)")
-                time.sleep(0.25)
+            driver.execute_script("window.scrollBy(0, " + str( (a * 50) % 1500 ) + ")")
+            time.sleep(0.25)
+            
+            element = driver.find_element(By.CLASS_NAME,"product-details-page-description__body")
+            return element.text
+            
+        except:
+            pass
 
-                driver.execute_script("window.scrollBy(0, " + str( (a * 50) % 1500 ) + ")")
-                time.sleep(0.25)
-                
-                elements = driver.find_elements_by_class_name('product-details-page-description__body')
-                if len(elements) > 0:
-                    return elements[0].text
-                
-            except:
-                pass
+    return "Could not find desc"
 
-def handleItem(item):
+number_threads = 4
+thread_duty = 672
+
+def handleThread(startVar):
     global finalizedWrites
-    item[5] = getDescription(item[7])
-    finalizedWrites.append(",".join(str(e).replace(",", "-").replace("\n", "[newline]") for e in item) + "\n")
+    global allItems
 
-number_threads = 15
+    with webdriver.Chrome(chcd, chrome_options=Chrome_options) as driver:
+        for a in range(thread_duty):
+            if startVar + a >= len(allItems):
+                return
+                
+            driver.get(allItems[startVar + a][7])
+            allItems[startVar + a][5] = getDescription(driver)
+            finalizedWrites.append(",".join(str(e).replace(",", "-").replace("\n", "[newline]") for e in allItems[startVar + a]) + "\n")
+
+
 def reformatCSV():
     global finalizedWrites
+    global allItems
+    startTime = time.time()
+
     csvfile = open("ExcelGeneration/Finalized_Bulk.csv", 'w')
     csvfile.write(",".join([
         "Category", "Product", "Product ID", "Price", "Old Price" , "Description", "Image URL", "Product URL"
     ]) + "\n")
     
     file = open("ExcelGeneration/Generated_Bulk.csv", "r")
-    allItems = [e.split(",") for e in file.read().split("\n")]
-    cur = 0
+    allItems = [e.split(",") for e in file.read().strip().split("\n")]
     endSpot = len(allItems)
+    cur = 1
 
-    while cur < 20:
-        if threading.active_count() < number_threads:
-            cur += 1
-            if (cur % 50 == 0):
-                print(" ======================= ", cur, " ======================= ")
-
-            x = threading.Thread(target=handleItem, args=(allItems[cur],))
-            x.start()
-            time.sleep(0.05)
-        else:
-            time.sleep(0.4)
+    for _ in range(number_threads):
+        x = threading.Thread(target=handleThread, args=(cur, ))
+        x.start()
+        cur += thread_duty
+        time.sleep(0.05)
     
     while threading.active_count() > 1:
-        print("Waiting to wrap up")
-        time.sleep(1)
+        print("COMPLETED: ", len(finalizedWrites))
+        time.sleep(3)
         
     for item in finalizedWrites:
         csvfile.write(item)
     
     file.close()
     csvfile.close()
+    endTime = time.time()
+    print("Finished making", len(finalizedWrites), "in", endTime-startTime, "seconds")
 
 def main():
     reformatCSV()
